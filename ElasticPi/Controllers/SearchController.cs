93d1a2f9244a512ed43039a836c1b733d868f5ae
Varun Gupta <Varun.Gupta3@honeywell.com>
2019-07-03 16:29:50 +0530

    Completed Search By API from the backend

--- ElasticPi/Controllers/SearchController.cs
+++ ElasticPi/Controllers/SearchController.cs
@@ -41,21 +41,81 @@ namespace ElasticPi.Controllers
         }
 
         [HttpGet("[action]")]
-        public IEnumerable<Data> GetBy(int size=10)
+        public IEnumerable<Data> GetBy(string systemGuid, string organizationId, string sensorId, int occupancyValue, int size = 10)
         {
             var settings = new ConnectionSettings(new Uri("http://10.8.173.181"))
             .DefaultIndex("ss-standardizedsensordata");
 
             var client = new ElasticClient(settings);
-            var searchResponse = client.Search<Data>(s => s
-                .Size(size)
-                .Query(q => q
-                    .MatchAll()
-                )
-            );
 
-            var data = searchResponse.Documents;
-            return data;
+            if (!String.IsNullOrEmpty(systemGuid))
+            {
+                var searchResponse = client.Search<Data>(s => s
+                    .Size(size)
+                    .Query(q => q
+                        .Match(m => m
+                            .Field(f => f.systemGuid)
+                            .Query(systemGuid)
+                        )
+                    )
+                );
+                var data = searchResponse.Documents;
+                return data;
+            }
+            else if (!String.IsNullOrEmpty(organizationId))
+            {
+                var searchResponse = client.Search<Data>(s => s
+                    .Size(size)
+                    .Query(q => q
+                        .Match(m => m
+                            .Field(f => f.organizationId)
+                            .Query(organizationId)
+                        )
+                    )
+                );
+                var data = searchResponse.Documents;
+                return data;
+            }
+            else if (!String.IsNullOrEmpty(sensorId))
+            {
+                var searchResponse = client.Search<Data>(s => s
+                    .Size(size)
+                    .Query(q => q
+                        .Match(m => m
+                            .Field(f => f.sensorId)
+                            .Query(sensorId)
+                        )
+                    )
+                );
+                var data = searchResponse.Documents;
+                return data;
+            }
+            else if (occupancyValue == 0 || occupancyValue == 1)
+            {
+                var searchResponse = client.Search<Data>(s => s
+                    .Size(size)
+                    .Query(q => q
+                        .Match(m => m
+                            .Field(f => f.occupancyValue)
+                            .Query(Convert.ToString(occupancyValue))
+                        )
+                    )
+                );
+                var data = searchResponse.Documents;
+                return data;
+            }
+            else
+            {
+                var searchResponse = client.Search<Data>(s => s
+                    .Size(size)
+                    .Query(q => q
+                        .MatchAll()
+                    )
+                );
+
+                var data = searchResponse.Documents;
+                return data;
+            }
         }
     }
 }
